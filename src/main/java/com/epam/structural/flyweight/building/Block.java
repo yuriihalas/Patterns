77adbbec3144e59a7652bb6a824466f948ce893c
Yurii Halas <yurii_halas@epam.com>
2019-09-23 16:52:30 +0300

    - removed useless java doc

--- src/main/java/com/epam/structural/flyweight/building/Block.java
+++ src/main/java/com/epam/structural/flyweight/building/Block.java
@@ -3,9 +3,6 @@ package com.epam.structural.flyweight.building;
 public class Block {
     private int possX;
     private int possY;
-    /**
-     * means of which blocks created building
-     */
     private BlockType blockType;
 
     public Block(int possX, int possY, BlockType blockType) {
